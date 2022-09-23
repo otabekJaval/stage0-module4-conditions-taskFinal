20370bbfd0c38c396710c4a6fb2dc2ca4a254e82
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-09-23 07:30:00 +0500

    Update DaysInMonth.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -1,7 +1,18 @@
 package school.mjc.stage0.conditions.finalTask;
 
+import java.util.GregorianCalendar;
+
 public class DaysInMonth {
     public void printDays(int year, int month) {
+
+        switch (month){
+            case 4,6,9,11-> System.out.println("30");
+            case 1,3,5,7,8,10,12-> System.out.println("31");
+            case 2-> System.out.println(new GregorianCalendar().isLeapYear(year)?"29":"28");
+            default -> System.out.println("invalid date");
+
+
+        }
 
     }
 }
